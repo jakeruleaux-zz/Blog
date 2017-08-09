94fb96138c5d979fe865d9f12b12bc1b1cf62b3e
Brittany Kerr and jacob ruleaux <brittany@>
2017-08-09 11:52:50 -0700

    have basic functionality with one bug in naming conventions

---
+++ app/components/update-review.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateReviewForm: false,
+actions: {
+  updateReviewForm() {
+    this.set('updateReviewForm', true);
+  },
+
+  update(review) {
+    var params = {
+      title: this.get('title'),
+      venue: this.get('venue'),
+      author: this.get('author'),
+      review: this.get('review'),
+      image: this.get('image')
+    };
+    this.set('updateReviewForm', false);
+    this.sendAction('update', review, params);
+  }
+}
+});
